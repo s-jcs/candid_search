526eb8f7c7fd6973761ca4f67ec443b18964db18
s-jcs <shosundberg@gmail.com>
2017-09-29 01:14:34 +0900

    :+1: added class method for filtering

--- lib/candid_search.rb
+++ lib/candid_search.rb
@@ -1,5 +1,30 @@
+require "active_support/concern"
+
 require "candid_search/version"
 
 module CandidSearch
-  # Your code goes here...
+  # ============================================================
+  # Original code by: Justin Weiss
+  # Code base from https://gist.github.com/justinweiss/9065666
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    # NOTE: Call the class method with a hash containing named scope, and search
+    #       key pairs. Always whitelist parameters before passing it to the method
+    def filter(filtering_params)
+      results = self.all # create an anonymous scope
+      filtering_params.each do |key, value|
+        next if value.blank?
+
+        # NOTE: if name contains `_or_` merge two conditions.
+        #       ex. Model.with_name_or_id
+        results = if key.to_s.include?('_or_')
+                    results.merge(results.public_send(key, value))
+                  else
+                    results.public_send(key, value)
+                  end
+      end
+      results
+    end
+  end
 end
